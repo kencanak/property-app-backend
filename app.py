d45c613d598c84e546939b705574b6310354a3bc
Kencana Kesuma <Apeshit@Kencanas-MacBook-Pro.local>
2016-06-07 21:27:20 +0800

    heroku procfile

--- app.py
+++ app.py
@@ -9,6 +9,16 @@ import jwt
 from eve.auth import TokenAuth
 import datetime
 
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
+
 def generate_salt():
     return uuid.uuid4().hex
 
